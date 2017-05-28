c907485a904e6f649acc64585bdd564b70e81dba
gilleslabs <gilles.labs.tosi@gmail.com>
2017-05-28 13:55:56 +0200

    fix registry

--- setup-registry.sh
+++ setup-registry.sh
@@ -2,7 +2,7 @@
 sudo mkdir -p /certs
 sudo openssl req -newkey rsa:4096 -nodes -sha256 -keyout /certs/domain.key \
         -x509 -days 3650 -out /certs/domain.crt -subj "/C=FR/L=Cannes/O=lab-swarm/CN=registry.infra.local"
-sudo mkdir -p /etc/docker/certs.d/registry.infra.local
+sudo mkdir -p /etc/docker/certs.d/registry.infra.local:5000
 sudo cp /certs/domain.crt /etc/docker/certs.d/registry.infra.local:5000/ca.crt
 sudo cp /certs/domain.crt /vagrant/demo/registry/certs/ca.crt
 sudo mkdir -p /auth
